c2dfa0effcb01afa6221e7ba43d6aedd3334e044
Diego Armando <dgo.npp@gmail.com>
2022-05-06 10:27:11 +0100

    Exemplo de Record

---
+++ UsingRecord/UsingRecord/Carro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingRecord
+{
+    public record Carro
+    {
+      
+
+        public string? Marca { get; init; }
+        public string? Modelo { get; init; }
+        public string? Cor { get; init; }
+        public string? Combustivel { get; init; }
+
+        public Carro(string? marca, string? modelo, string? cor, string? combustivel)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+            Combustivel = combustivel;
+        }
+
+        public void Deconstruct(out string? marca, out string? modelo, out string? cor, out string? combustivel)
+        {
+            marca = Marca;
+            modelo = Modelo;
+            cor = Cor;  
+            combustivel = Combustivel;
+        }
+
+
+
+    }
+}
---
+++ UsingRecord/UsingRecord/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UsingRecord;
+
+Console.WriteLine("Hello, Example Record");
+
+var carro = new Carro(
+    "Ford",
+    "Fiesta",
+    "Branco",
+    "Gasolina"
+);
+
+
+Console.WriteLine($"Carro Inicial: {carro}");
+
+/*NOVO CARRO*/
+var novoCarro = carro with { Modelo = "Focus" };
+
+Console.WriteLine($"Novo Carro: {novoCarro}");
+
+/*COMPARAÇÃO*/
+Console.WriteLine($"Carro é igual ao Novo Carro: {carro == novoCarro}");
+novoCarro = carro with { Modelo = "Fiesta" };
+Console.WriteLine($"Carro é igual ao Novo Carro: {carro == novoCarro}");
+
+/*DECONSTRUCT*/
+var (marca, modelo, cor, combustivel) = novoCarro;
+Console.WriteLine($"Marca: {marca}");
+Console.WriteLine($"Modelo: {modelo}");
+Console.WriteLine($"Cor:{cor}");
+Console.WriteLine($"Combustivel:{combustivel}");
+
+
+
