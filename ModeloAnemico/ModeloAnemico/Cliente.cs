0b0d0076a42a0cc0e301d8445c985d74f7b151ae
Diego Armando <dgo.npp@gmail.com>
2022-05-23 20:24:07 +0100

    Update

---
+++ ModeloAnemico/ModeloAnemico/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloAnemico
+{
+    //Sealed cannot be inherited
+    public sealed class Cliente
+    {
+        public Cliente(Guid id, string name, string lastName)
+        {
+            Validate(id, name, lastName);
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+
+        private void Validate(Guid id, string name, string lastName)
+        {
+            if(name == null || name.Length < 3)
+            {
+                throw new InvalidOperationException("The name is required and must be greater than 3");
+            }
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public string LastName { get; private set; }
+        
+
+
+        
+
+    }
+}
---
+++ ModeloAnemico/ModeloAnemico/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ModeloAnemico;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    var cliente = new Cliente(Guid.NewGuid(), "Di", "Bento");
+    Console.WriteLine(cliente.Name);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+
