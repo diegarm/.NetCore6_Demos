37b7807580363da3f5e86eae09982455f218e8e5
Diego Armando <39356591+diegarm@users.noreply.github.com>
2022-07-28 21:23:23 +0100

    Update

--- CQRS/VendingMachine.Application/Interfaces/IVendingService.cs
+++ CQRS/VendingMachine.Application/Interfaces/IVendingService.cs
@@ -1,10 +1,11 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using VendingMachine.Application.ViewModels;
+
 namespace VendingMachine.Application.Interface
 {
     public interface IVendingService : IDisposable
     {
-      
-
-
+        Task<ValidationResult> RegisterCashIn(CashInViewModel cashIn);
     }
 }
---
+++ CQRS/VendingMachine.Application/ViewModels/CashInViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Application.ViewModels
+{
+    public class CashInViewModel
+    {
+        [Required(ErrorMessage = "The amount is required")]
+        [MinLength(1)]
+        public decimal Amount;
+    }
+}
