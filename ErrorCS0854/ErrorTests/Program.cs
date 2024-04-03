44f6d053a3a0da697afbf9662f5e36d9134f4263
Diego Armando <darmando@whitestar.pt>
2024-04-03 16:42:07 +0100

    Demos

---
+++ ErrorCS0854/ErrorTests/Program.cs
@@ -0,0 +1,14 @@
+﻿// CS0854.cs (10,48)
+using System;
+using System.Linq.Expressions;
+
+public class Test
+{
+    public static int ModAdd2(int x = 3, int y = 4, params int[] b) { return 0; }
+
+    static void Main()
+    {
+        Expression<Func<int>> testExpr = () => ModAdd2();
+        Console.WriteLine(testExpr);
+    }
+}
